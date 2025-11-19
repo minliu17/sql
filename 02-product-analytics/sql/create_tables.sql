2a80a94b147e9219f6b4777ca78dcd2da68978f0
minliu17 <94867946+minliu17@users.noreply.github.com>
2025-11-19 11:30:24 -0800

    Increase precision of conversion_rate field

--- 02-product-analytics/sql/create_tables.sql
+++ 02-product-analytics/sql/create_tables.sql
@@ -1,11 +1,10 @@
 CREATE TABLE product_events (
-    event_id INT PRIMARY KEY,
     event_date DATE,
     device_type VARCHAR(50),
     marketing_channel VARCHAR(50),
     page_views INT,
     add_to_cart INT,
     purchases INT,
-    conversion_rate DECIMAL(5,4)
+    conversion_rate DECIMAL(10,4)
 );
 
