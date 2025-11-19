1e4d8cf6b9913a3323fd773b0b4e13fd3a88d128
minliu17 <94867946+minliu17@users.noreply.github.com>
2025-11-19 11:26:46 -0800

    Update cohort_analysis.sql

--- 01-saas-revenue-cohort/sql/cohort_analysis.sql
+++ 01-saas-revenue-cohort/sql/cohort_analysis.sql
@@ -1,16 +1,44 @@
--- Example: Monthly Cohort Retention
-WITH first_month AS (
-    SELECT customer_id,
-           MIN(DATE_TRUNC('month', signup_date)) AS cohort_month
+/* Build monthly cohort groups */
+WITH customer_cohort AS (
+    SELECT
+        customer_id,
+        DATE_TRUNC('month', signup_date) AS cohort_month
     FROM customers
-    GROUP BY customer_id
+),
+
+activity AS (
+    SELECT
+        c.customer_id,
+        DATE_TRUNC('month', t.transaction_date) AS active_month,
+        cc.cohort_month
+    FROM transactions t
+    JOIN subscriptions s ON t.subscription_id = s.subscription_id
+    JOIN customers c ON s.customer_id = c.customer_id
+    JOIN customer_cohort cc ON c.customer_id = cc.customer_id
+),
+
+cohort_calc AS (
+    SELECT
+        cohort_month,
+        active_month,
+        COUNT(DISTINCT customer_id) AS active_customers
+    FROM activity
+    GROUP BY 1,2
+),
+
+cohort_index AS (
+    SELECT
+        cohort_month,
+        active_month,
+        active_customers,
+        EXTRACT(
+            MONTH FROM (active_month - cohort_month)
+        ) AS months_since_signup
+    FROM cohort_calc
 )
-SELECT f.cohort_month,
-       DATE_TRUNC('month', t.transaction_date) AS month,
-       COUNT(DISTINCT t.customer_id) AS active_customers
-FROM first_month f
-JOIN subscriptions s ON s.customer_id = f.customer_id
-JOIN transactions t ON t.subscription_id = s.subscription_id
-GROUP BY f.cohort_month, DATE_TRUNC('month', t.transaction_date)
-ORDER BY f.cohort_month, month;
+
+SELECT *
+FROM cohort_index
+ORDER BY cohort_month, months_since_signup;
+
 
